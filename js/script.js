5abe8084ecc9d516091ecbf0400702824ddfd944
GiordanoDiego <diegomgiordano96@gmail.com>
2024-01-09 10:51:14 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 const {createApp} = Vue;
 
 createApp({
-    data() {
+    data() { // creo le variabili visibili nella parte montata
     return {
-        message: 'Hello Vue! ',
+        currentActive: 0, 
+        indiceThumb: 0,
+        //contatore che implementerò per cambiare slides
         slides: [
             {
-                image: 'img/01.webp',
+                    image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                 }, {
@@ -28,7 +30,33 @@ createApp({
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
             ],
+        }   
+    }, methods:{ //creo le funzioni
+        prevSlides(){
+            if(this.currentActive > 0){
+                this.currentActive--; 
+                }
+                else {
+                this.currentActive = this.slides.lenght -1;}
+
+        },
+        nextSlides(){
+            if(this.currentActive < this.slides.lenght -1){
+                this.currentActive++;
+                console.log("dentro if", this.currentActive);
+            }else{
+                this.currentActive = 0;
+                console.log("dentro else",this.currentActive);
+            }
+        },
+        getActiveSlides(){
+            if(this.currentActive == indiceThumb){
+                return 'active';
+            }else{
+                return '';
+            }
+        }
     }
-}
+
 }).mount('#app');
 
